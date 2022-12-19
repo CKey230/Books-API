554e24675af80448272a7aee62c4250fd4e38bef
Ckey230 <Ckey230@gmail.com>
2022-12-19 18:41:34 -0500

    home route and 404 route

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+})
+
+app.get('*', (req,res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+    console.log(`Currently listening on ${process.env.PORT}..`)
+
