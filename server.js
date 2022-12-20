3ab6582d800c6b7f688cf4876a3642115c00f291
Ckey230 <Ckey230@gmail.com>
2022-12-19 19:30:31 -0500

    default

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ require('dotenv').config()
 
 const app = express()
 
+//middleware
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'jsx')
+app.engine('jsx',require('express-react-views').createEngine())
+
 app.use('/books', require('./controllers/books'))
 
 
@@ -13,6 +19,7 @@ app.get('/', (req,res) => {
 app.get('*', (req,res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
+const PORT = process.env.PORT || 8080
 
 app.listen(process.env.PORT)
     console.log(`Currently listening on ${process.env.PORT}..`)
